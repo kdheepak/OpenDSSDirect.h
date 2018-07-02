14a867be6082327dcfce0d7bbd1ee97ae0343aed
Dheepak Krishnamurthy <kdheepak89@gmail.com>
2018-07-02 09:01:09 -0600

    Update schema

--- scripts/generate_headers.py
+++ scripts/generate_headers.py
@@ -14,16 +14,21 @@ def main():
     func_sigs = []
     for filename in files:
         with open(os.path.join(path_to_folder, filename)) as f:
+            read_lines = False
             data = f.read()
             for l in data.splitlines():
-                if "cdecl" in l:
+                if "interface" in l:
+                    read_lines = True
+                if "implementation" in l:
+                    read_lines = False
+                if read_lines is True and "cdecl" in l:
+                    l = l.replace("Function", "function").replace("procedure", "function").replace("Procedure", "function").replace("  ", " ")
                     func_sigs.append(l)
 
-    os.path.abspath("./../")
-
-    with open("./../opendssdirect.pas", "w") as f:
+    func_sigs = sorted(func_sigs)
+    with open(os.path.abspath(os.path.join(current_directory, "./../opendssdirect.pas")), "w") as f:
         f.write("\n".join(func_sigs))
 
-if __name__ == "__main__":
 
+if __name__ == "__main__":
     main()
