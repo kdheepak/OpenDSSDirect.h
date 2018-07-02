fc17fd6df35b747b94ea8c0546303dd9ffe8531c
Dheepak Krishnamurthy <kdheepak89@gmail.com>
2018-07-02 09:52:00 -0600

    Add opendssdirect.json

--- scripts/main.py
+++ scripts/main.py
@@ -1,4 +1,5 @@
 import os
+import json
 
 current_directory = os.path.realpath(os.path.dirname(__file__))
 
@@ -32,7 +33,48 @@ def generate_headers():
 
 def parse_headers():
 
-    pass
+    with open(os.path.join(current_directory, "../opendssdirect.pas")) as f:
+        data = f.read()
+
+    opendss_functions = []
+    for l in data.splitlines():
+
+        # get function name
+        name = l.split("(")[0].strip("function ")
+
+        # get output type
+        if "(" not in l and ")" not in l:
+            output_type = "void"
+        else:
+            output_type = l.split(")")[-1]
+            if output_type.startswith(":"):
+                output_type = output_type.split(";")[0].split(":")[-1].strip()
+            else:
+                output_type = "void"
+
+        # get input arguments
+        if "(" not in l and ")" not in l or l.split("(")[1].split(")")[0].strip() == "":
+            input_arguments = {"number": 0, "type": []}
+        else:
+            arguments = []
+            input_arguments = l.split("(")[1].split(")")[0]
+            for arg in input_arguments.split(";"):
+                # only a single argument
+                arg_names, arg_type = arg.split(":")
+                for arg_name in arg_names.split(","):
+                    d = {"name": arg_name.strip().replace("var ", "").replace("out ", ""), "type": arg_type.strip()}
+                    arguments.append(d)
+
+        function_definition = {
+            "name": name,
+            "output": {"type": output_type},
+            "input": arguments
+        }
+
+        opendss_functions.append(function_definition)
+
+    with open(os.path.join(current_directory, "../opendssdirect.json"), "w") as f:
+        f.write(json.dumps(opendss_functions, indent=4, separators={",": "", ":": ""}))
 
 
 if __name__ == "__main__":
