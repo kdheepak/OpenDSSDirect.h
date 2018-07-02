6499822c4c9450b90bb20c80fb62a217eaaa74e5
Dheepak Krishnamurthy <kdheepak89@gmail.com>
2018-07-02 13:15:59 -0600

    Fix some duplicate names

--- scripts/main.py
+++ scripts/main.py
@@ -27,9 +27,9 @@ def generate_headers():
             read_lines = False
             data = f.read()
             for l in data.splitlines():
-                if "interface" in l:
+                if l.strip().lower().startswith("interface"):
                     read_lines = True
-                if "implementation" in l:
+                if l.strip().lower().startswith("implementation"):
                     read_lines = False
                 if read_lines is True and "cdecl" in l:
                     l = l.replace("Function", "function").replace("procedure", "function").replace("Procedure", "function").replace("  ", " ")
